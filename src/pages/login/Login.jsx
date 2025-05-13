9ebfdebf89feec8f0b878b02bb71a2b9fa575399
Mohd Arif <s.arifsaifi000@gmail.com>
2025-05-13 12:47:01 +0530

    login screen is completed

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import LoginCard from '../../components/LoginCard/LoginCard';
-import './Login.scss';
+import React from "react";
+import LoginCard from "../../components/LoginCard/LoginCard";
+import "./Login.scss";
 
 const Login = () => {
   return (
-    <div className='login-container'>
-     <div className="left-content">
+    <div className="login-container">
+      <div className="left-content">
+        <h1 className="poppins-font-family">Start your journey with us.</h1>
+        <p className="poppins-font-family font-size-md">
+          It brings together your tasks,
+          <br />
+          projects, timelines, files and more.
+        </p> 
+      </div>
 
-     </div>
-
-     <div className="right-content">
-         <LoginCard/>
-     </div>
+      <div className="right-content">
+        <LoginCard />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
